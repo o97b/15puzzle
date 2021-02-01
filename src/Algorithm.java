36583a20b0a3fd2c0c69b5b9922ee6b33977596a
o97b <unxxxt@gmail.ru>
2021-02-01 19:41:46 +0300

    queue with hashcode

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -7,24 +7,25 @@ public class Algorithm {
     static HashMap<Integer, Position> mapOfVisited = new HashMap<>();
     static LinkedList<Position> path = new LinkedList<>();
 
-    static PriorityQueue<Position> priorityQueue= new PriorityQueue<>(new Comparator<Position>() {
+    static PriorityQueue<Integer> priorityQueue= new PriorityQueue<>(new Comparator<Integer>() {
         @Override
-        public int compare(Position o1, Position o2) {
-            return Integer.compare(o1.weight, o2.weight);
+        public int compare(Integer o1, Integer o2) {
+            return Integer.compare(mapOfVisited.get(o1).weight, mapOfVisited.get(o2).weight);
         }
     });
 
     public static LinkedList<Position> algorithm(Position currentPosition) {
-        priorityQueue.add(currentPosition);
+        priorityQueue.add(currentPosition.hashCode());
         ArrayList<Integer> visitedArrayPosition;
         List<Position> checkedPosition = new LinkedList<>();
         List<Position> neighbors = new LinkedList<>();
+        mapOfVisited.put(currentPosition.hashCode(), currentPosition);
 
         while (!priorityQueue.isEmpty()) {
 //        for( int i = 0; i < 5; i++) {
-            currentPosition = priorityQueue.poll();
+            currentPosition = mapOfVisited.get(priorityQueue.poll());
             System.out.println(currentPosition.positionArray.toString());
-            mapOfVisited.put(currentPosition.positionArray.hashCode(), currentPosition);
+            mapOfVisited.put(currentPosition.hashCode(), currentPosition);
 
             if (currentPosition.positionArray.equals(Main.endPosition.positionArray)) {
                 Position parent = currentPosition.parent;
@@ -35,22 +36,20 @@ public class Algorithm {
                 path.add(Main.endPosition);
                 return path;
             }
-            checkedPosition.add(currentPosition); // Окрашиваем в черный
+            checkedPosition.add(currentPosition);
 
             neighbors = currentPosition.neighbors();
             for(Position position : neighbors) {
-                int key = position.positionArray.hashCode();
+                int key = position.hashCode();
                 if (mapOfVisited.containsKey(key)) {
                     Position previousPosition = mapOfVisited.get(key);
                     if (previousPosition.stepForPosition > position.stepForPosition) {
                         mapOfVisited.put(key, position);
-                        priorityQueue.remove(previousPosition);
-                        priorityQueue.add(position);
                     }
                 } else {
-                    priorityQueue.add(position);
-                    System.out.println("ADD " + position.positionArray.toString());
                     mapOfVisited.put(key, position);
+                    priorityQueue.add(position.hashCode());
+                    System.out.println("ADD " + position.positionArray.toString());
                 }
             }
             System.out.println("-------------");
